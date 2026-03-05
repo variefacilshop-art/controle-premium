62ffa8a034466b3168b89e3b14e5bd644be18060
variefacilshop-art <variefacilshop@gmail.com>
2026-03-04 23:07:11 -0300

    Update vendas.js

--- js/vendas.js
+++ js/vendas.js
@@ -1,7 +1,24 @@
-// ========================
+// ============================
 // MODULO VENDAS
-// ========================
+// ============================
 
-function initVendas(){
-    console.log("Modulo vendas carregado");
+const tabVendas = document.getElementById("tabVendas");
+const vendasBox = document.getElementById("vendasBox");
+
+const financasBox = document.getElementById("financasBox");
+const metasBox = document.getElementById("metasBox");
+const dashboardBox = document.getElementById("dashboardBox");
+
+if (tabVendas) {
+
+  tabVendas.onclick = () => {
+
+    if (financasBox) financasBox.classList.add("hidden");
+    if (metasBox) metasBox.classList.add("hidden");
+    if (dashboardBox) dashboardBox.classList.add("hidden");
+
+    if (vendasBox) vendasBox.classList.remove("hidden");
+
+  };
+
 }
