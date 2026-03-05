2146c79798364c59753ed9b190462fe4990b9405
variefacilshop-art <variefacilshop@gmail.com>
2026-03-04 23:16:22 -0300

    Update vendas.js

--- js/vendas.js
+++ js/vendas.js
@@ -8,6 +8,12 @@ if(tabVendas){
 
   tabVendas.addEventListener("click", ()=>{
 
+    document.querySelectorAll(".tab").forEach(t=>{
+      t.classList.remove("active");
+    });
+
+    tabVendas.classList.add("active");
+
     document.getElementById("financasBox")?.classList.add("hidden");
     document.getElementById("metasBox")?.classList.add("hidden");
     document.getElementById("dashboardBox")?.classList.add("hidden");
